6b75a72a8e523651c074a1e4acb83a3288eaefcf
aoxinhu <aoxinhu@gmail.com>
2022-02-11 17:05:20 +0800

    v2.11-amost-runnable

--- trend-trading-backtest-service/src/main/java/org/aoxinhu/trend/web/BackTestController.java
+++ trend-trading-backtest-service/src/main/java/org/aoxinhu/trend/web/BackTestController.java
@@ -7,7 +7,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
- 
+
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.StrUtil;
+
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,12 +21,51 @@ import java.util.Map;
 public class BackTestController {
     @Autowired BackTestService backTestService;
  
-    @GetMapping("/simulate/{code}")
+    // @GetMapping("/simulate/{code}")
+    // @CrossOrigin
+    // public Map<String,Object> backTest(@PathVariable("code") String code) throws Exception {
+    //     List<IndexData> allIndexDatas = backTestService.listIndexData(code);
+    //     Map<String,Object> result = new HashMap<>();
+    //     result.put("indexDatas", allIndexDatas);
+    //     return result;
+    // }
+
+    @GetMapping("/simulate/{code}/{startDate}/{endDate}")
     @CrossOrigin
-    public Map<String,Object> backTest(@PathVariable("code") String code) throws Exception {
+    public Map<String,Object> backTest(
+            @PathVariable("code") String code
+            ,@PathVariable("startDate") String strStartDate
+            ,@PathVariable("endDate") String strEndDate
+    ) throws Exception {
         List<IndexData> allIndexDatas = backTestService.listIndexData(code);
+ 
+        String indexStartDate = allIndexDatas.get(0).getDate();
+        String indexEndDate = allIndexDatas.get(allIndexDatas.size()-1).getDate();
+ 
+        allIndexDatas = filterByDateRange(allIndexDatas,strStartDate, strEndDate);
+ 
         Map<String,Object> result = new HashMap<>();
         result.put("indexDatas", allIndexDatas);
+        result.put("indexStartDate", indexStartDate);
+        result.put("indexEndDate", indexEndDate);
+ 
+        return result;
+    }
+
+    private List<IndexData> filterByDateRange(List<IndexData> allIndexDatas, String strStartDate, String strEndDate) {
+        if(StrUtil.isBlankOrUndefined(strStartDate) || StrUtil.isBlankOrUndefined(strEndDate) )
+            return allIndexDatas;
+ 
+        List<IndexData> result = new ArrayList<>();
+        Date startDate = DateUtil.parse(strStartDate);
+        Date endDate = DateUtil.parse(strEndDate);
+ 
+        for (IndexData indexData : allIndexDatas) {
+            Date date =DateUtil.parse(indexData.getDate());
+            if(date.getTime()>=startDate.getTime() && date.getTime()<=endDate.getTime()) {
+                result.add(indexData);
+            }
+        }
         return result;
     }
 }
