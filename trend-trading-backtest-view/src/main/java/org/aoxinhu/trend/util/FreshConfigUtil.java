e436dd849d537c96b110a3749ba737ecae6b7440
aoxinhu <aoxinhu@gmail.com>
2023-09-16 22:18:19 +0800

    9.16.2023

--- trend-trading-backtest-view/src/main/java/org/aoxinhu/trend/util/FreshConfigUtil.java
+++ trend-trading-backtest-view/src/main/java/org/aoxinhu/trend/util/FreshConfigUtil.java
@@ -7,12 +7,12 @@ import cn.hutool.http.HttpUtil;
 public class FreshConfigUtil {
   
     public static void main(String[] args) {
-        HashMap<String,String> headers =new HashMap<>();
-        headers.put("Content-Type", "application/json; charset=utf-8");
-        System.out.println("因为要去git获取，还要刷新index-config-server, 会比较卡，所以一般会要好几秒才能完成，请耐心等待");
+        // HashMap<String,String> headers =new HashMap<>();
+        // headers.put("Content-Type", "application/json; charset=utf-8");
+        // System.out.println("因为要去git获取，还要刷新index-config-server, 会比较卡，所以一般会要好几秒才能完成，请耐心等待");
   
-        String result = HttpUtil.createPost("http://localhost:8041/actuator/bus-refresh").addHeaders(headers).execute().body();
-        System.out.println("result:"+result);
-        System.out.println("refresh 完成");
+        // String result = HttpUtil.createPost("http://localhost:8041/actuator/bus-refresh").addHeaders(headers).execute().body();
+        // System.out.println("result:"+result);
+        // System.out.println("refresh 完成");
     }
 }
